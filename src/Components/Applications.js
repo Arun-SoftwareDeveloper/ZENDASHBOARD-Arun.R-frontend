634e6ffb48063e2fc65b5199cb7c2dd6f7dace49
Arun-SoftwareDeveloper <arunramasamy46@gmail.com>
2024-01-27 11:33:23 +0530

    new file:   public/netlify.toml 	modified:   src/Components/Applications.js

--- src/Components/Applications.js
+++ src/Components/Applications.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import SidebarHeaderContainer from '../Components/SidebarHeaderContainer';
-import '../Styles/Applications.css';
+import React from "react";
+import SidebarHeaderContainer from "../Components/SidebarHeaderContainer";
+import "../Styles/Applications.css";
 
 function Applications() {
   return (
-    <div className='application-container'>
-      <SidebarHeaderContainer headerTitle='Applications' />
-      <h1 className='application-text'>
+    <div className="application-container">
+      <SidebarHeaderContainer headerTitle="Applications" />
+      <h1 className="application-text">
         Content available after completing Placement preparation...
       </h1>
     </div>
@@ -14,4 +14,3 @@ function Applications() {
 }
 
 export default Applications;
-
